a1372d978066914cb32ca2305ee77add853b8760
ducnmhe141262 <80652862+ducnmhe141262@users.noreply.github.com>
2021-03-27 21:56:48 +0700

    Add files via upload

---
+++ controller/loginControll.java
@@ -0,0 +1,98 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controll;
+
+import dal.LoginDAO;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import model.Login;
+
+/**
+ *
+ * @author Uchitachi
+ */
+public class loginControll extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+   
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        request.getRequestDispatcher("login.jsp").forward(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String username = request.getParameter("user");
+        String password = request.getParameter("pass");
+        String remember = request.getParameter("remember");
+        
+        LoginDAO db = new LoginDAO();
+        Login account = db.getAccountByUserNameandPassword(username, password);
+        if(account != null)
+        {
+            request.getSession().setAttribute("user", account);
+            if(remember!= null && remember.equals("remember"))
+            {
+                Cookie user = new Cookie("cuser", username);
+                Cookie pass = new Cookie("cpass", password);
+                user.setMaxAge(60 * 60 * 24);
+                pass.setMaxAge(60 * 60 * 24);
+                response.addCookie(pass);
+                response.addCookie(user);
+            }
+            
+            response.sendRedirect("client/list");
+        }
+        else
+        {
+            response.getWriter().println("invalid username/password.");
+        }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
